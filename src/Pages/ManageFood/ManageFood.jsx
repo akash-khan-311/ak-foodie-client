5c7ae91a31c0f35af4886dd554efb6e53b96d5b2
Akash Khan <akashcc48@gmail.com>
2023-11-05 22:46:12 +0600

    manage section data has been loaded

--- src/Pages/ManageFood/ManageFood.jsx
+++ src/Pages/ManageFood/ManageFood.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Headroom from "react-headroom";
 import Header from "../../Shared/Header/Header";
 
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+
 const ManageFood = () => {
+  const { user } = useContext(AuthContext);
+  const [myFoods, setMyFoods] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/v1/myfood?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setMyFoods(data));
+  }, [user]);
+
   return (
     <div>
       <Headroom>
         <Header />
       </Headroom>
+
+      <div>
+        <h1 className="text-6xl text-white">{myFoods?.length}</h1>
+      </div>
     </div>
   );
 };
