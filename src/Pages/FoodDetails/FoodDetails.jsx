a7ac75dd7298e3b0558871364a14f201cc280e30
Akash Khan <akashcc48@gmail.com>
2023-11-06 18:30:38 +0600

    requested data loaded

--- src/Pages/FoodDetails/FoodDetails.jsx
+++ src/Pages/FoodDetails/FoodDetails.jsx
@@ -11,6 +11,8 @@ import {
   DialogFooter,
 } from "@material-tailwind/react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
+import axios from "axios";
+import swal from "sweetalert";
 
 const FoodDetails = () => {
   const food = useLoaderData();
@@ -55,8 +57,19 @@ const FoodDetails = () => {
       requesterImg: user?.photoURL,
       requesterEmail: user.email,
       requestDate,
+      requested: true,
     };
-    console.log(requestFood);
+
+    axios
+      .post("http://localhost:3000/api/v1/requestfood", requestFood)
+      .then((res) => {
+        if (res.data.acknowledged) {
+          swal("Good job!", "Food Request Successfully", "success");
+          handleOpen(null)
+        } else {
+          swal("Oops!", " Something went wrong", "error");
+        }
+      });
   };
 
   const currentDate = new Date();
@@ -280,7 +293,6 @@ const FoodDetails = () => {
                           placeholder=" "
                           name="notes"
                           type="text"
-                        
                         />
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-gray-300 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-teal-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-teal-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-teal-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                           Aditional Notes
--- src/Pages/FoodRequest/FoodRequest.jsx
+++ src/Pages/FoodRequest/FoodRequest.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Headroom from "react-headroom";
 import Header from "../../Shared/Header/Header";
+import axios from "axios";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const FoodRequest = () => {
+  const [requestedFood, setRequestedFood] = useState([]);
+  const { user } = useContext(AuthContext);
+  const email = user.email;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/v1/requestfood/${email}`)
+      .then((res) => {
+        setRequestedFood(res.data);
+      });
+  }, []);
   return (
     <div>
       <Headroom>
         <Header />
       </Headroom>
-      <div className="my-10"></div>
+      <div className="my-10 container mx-auto">
+        <h1 className="text-white">{requestedFood.length}</h1>
+      </div>
     </div>
   );
 };
